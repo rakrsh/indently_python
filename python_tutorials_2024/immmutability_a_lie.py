f15376197eb6f9a498418d919d753af356340f0f
ravikr1 <ravikr731@gmail.com>
2024-08-17 23:29:41 +0530

    Added pre-commit config

--- python_tutorials_2024/immmutability_a_lie.py
+++ python_tutorials_2024/immmutability_a_lie.py
@@ -1,8 +1,9 @@
 # Example of tuple and string
+from __future__ import annotations
+
 from typing import Tuple as tuple
 
 coordinates: tuple[float, float] = (1, 2)
-print("Before: ", id(coordinates))
-coordinates += 3,
-print("After: ", id(coordinates))
-
+print('Before: ', id(coordinates))
+coordinates += 3,  # type: ignore
+print('After: ', id(coordinates))
