47780fd7129add5640ff2fb1972a5297da6629c8
ravikr1 <ravikr731@gmail.com>
2024-08-23 07:22:30 +0530

    Add set operations

--- python_tutorials_2024/operations.py
+++ python_tutorials_2024/operations.py
@@ -1,7 +1,18 @@
 from __future__ import annotations
+
+
 numbers: list[int] = list(range(1, 11))
 text: str = 'Hello World!'
 
 rev: slice = slice(None, None, -1)
 print(numbers[rev])
 print(text[rev])
+
+
+# Set operations
+set_a: set[int] = {1, 2, 3, 4, 5}
+set_b: set[int] = {4, 5, 6, 7, 8}
+
+print(f"{set_a | set_b}")
+print(f"{set_a - set_b}")
+print(f"{set_b - set_a}")
